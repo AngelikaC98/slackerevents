c82a4330eaa60e276d46ddd4226168477a8f4c27
crds83 <119341646+crds83@users.noreply.github.com>
2025-07-30 18:03:03 +0000

    deleted the LoginModal file as it is useless

--- client/src/components/Navigation/navigation.tsx
+++ client/src/components/Navigation/navigation.tsx
@@ -17,7 +17,7 @@ import Profile2 from "../../../public/assets/icons/profile2.jsx";
 import Button from "@/components/UI/UniversalButton/button";
 import Footer from "@/components/Footer/Footer";
 import MobileMenuFooter from "./MobileMenuFooter";
-import LoginModal from "@/components/LoginModal/LoginModal";
+
 import { useTranslation } from "@/hooks/useTranslation";
 // ------------ Styling ---------------
 import "./navigation.styled.css";
@@ -53,7 +53,7 @@ const Navigation: React.FC = () => {
 
   // Routing and session hooks
   const pathname = usePathname();
-  // const router = useRouter();
+  const router = useRouter();
   const { data: session } = useSession();
   const isLoggedIn = !!session;
 
@@ -143,7 +143,7 @@ const Navigation: React.FC = () => {
                 <Button
                   onClick={() => {
                     if (!isLoggedIn) {
-                      setLoginModalOpen(true);
+                      router.push("/login"); // Navigate to login page
                     } else {
                       setUserMenuOpen((value) => !value);
                     }
@@ -286,14 +286,6 @@ const Navigation: React.FC = () => {
           <Footer />
         </div>
       )}
-
-      {/* Login Modal */}
-      <LoginModal
-        isOpen={loginModalOpen}
-        onClose={() => setLoginModalOpen(false)}
-        onBack={() => setLoginModalOpen(false)}
-        callbackUrl={pathname || "/"}
-      />
     </>
   );
 };
